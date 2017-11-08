d9f09db2f3eedf2adee064ce0f615caec9a79851
mgppgg <32339510+mgppgg@users.noreply.github.com>
2017-11-08 14:23:10 +0100

    subir foto conseguido

--- app/src/main/java/mgppgg/tioney/Publicar.java
+++ app/src/main/java/mgppgg/tioney/Publicar.java
@@ -2,12 +2,15 @@ package mgppgg.tioney;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -17,14 +20,19 @@ import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import static android.R.attr.data;
 
 
 public class Publicar extends AppCompatActivity {
 
     private Button BtnSubir;
-    UploadTask uploadTask;
-    FirebaseStorage storage = FirebaseStorage.getInstance();
+    //UploadTask uploadTask;
+    //FirebaseStorage storage = FirebaseStorage.getInstance();
     private StorageReference storageRef;
+    private static final int GALLERY_INTENT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +41,15 @@ public class Publicar extends AppCompatActivity {
 
         BtnSubir = (Button)findViewById(R.id.BtnSubir);
 
+        storageRef = FirebaseStorage.getInstance().getReference();
+
         BtnSubir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                storageRef = storage.getReference();
+                abrirGaleria();
+
+                /*storageRef = storage.getReference();
 
                 Uri file = Uri.fromFile(new File("0/DCIM/Camera/IMG_20160724_161338.jpg"));
                 StorageReference riversRef = storageRef.child("images/"+file.getLastPathSegment());
@@ -55,11 +67,37 @@ public class Publicar extends AppCompatActivity {
                         // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
                         @SuppressWarnings("VisibleForTests")Uri downloadUrl = taskSnapshot.getDownloadUrl();
                     }
-                });
+                });*/
 
 
             }
         });
 
     }
+
+
+    public void abrirGaleria(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent, "Seleccione imagenes"),GALLERY_INTENT);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        if(requestCode==GALLERY_INTENT && resultCode==RESULT_OK){
+            Uri uri = imageReturnedIntent.getData();
+            StorageReference filepath = storageRef.child("fotos").child(uri.getLastPathSegment());
+
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                    Toast.makeText(Publicar.this, "Subida correcta", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+        }
+    }
+
 }
