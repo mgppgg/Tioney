3c8aa49c674ac01ff5632a77ed93100af19fd656
mgppgg <32339510+mgppgg@users.noreply.github.com>
2018-01-20 14:18:25 +0100

    filtro 2

--- app/src/main/java/mgppgg/tioney/MainActivity.java
+++ app/src/main/java/mgppgg/tioney/MainActivity.java
@@ -31,7 +31,11 @@ import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -268,9 +272,60 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
 
         final Dialog dialog = new Dialog(context);
         dialog.setContentView(R.layout.filtro);
+        int desde,hasta;
         Button BTNaplicar = (Button)dialog.findViewById(R.id.BTNaplicar);
         Button BTNcancelar = (Button)dialog.findViewById(R.id.BTNcancelar);
+        final EditText ETdesde = (EditText)dialog.findViewById(R.id.ETdesde);
+        final EditText EThasta = (EditText)dialog.findViewById(R.id.EThasta);
+        final TextView TVdistancia = (TextView)dialog.findViewById(R.id.TV_fil_numdis);
+        final TextView mm = (TextView)dialog.findViewById(R.id.TV_fil_distancia);
+        final SeekBar distancia = (SeekBar)dialog.findViewById(R.id.SBdistancia);
+        Spinner spinner = (Spinner)dialog.findViewById(R.id.spinner);
 
+        distancia.setMax(3);
+        distancia.setProgress(1);
+        if(!ETdesde.getText().toString().isEmpty())desde = Integer.parseInt(ETdesde.getText().toString());
+        if(!EThasta.getText().toString().isEmpty())hasta = Integer.parseInt(EThasta.getText().toString());
+
+        distancia.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mm.setText("Distancia:   <");
+                if(progress==0)progress=1;
+                else if(progress==1)progress=5;
+                     else if(progress==2 )progress=10;
+                          else if(progress==3 ){
+                                progress=10;
+                                mm.setText("Distancia:   >");
+                          }
+                TVdistancia.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+                if (pos == 0) {
+                }else {
+                    // Your code to process the selection
+                }
+            }
+        });
 
 
         BTNaplicar.setOnClickListener(new View.OnClickListener() {
